9b1f1604fc4fde94bd2f83cda800719c3bb27299
Stualyttle <Stualyttle@users.noreply.github.com>
2022-10-06 22:31:41 +0200

    Fix install stopping

--- Git/versions/current/.lyttle_tools/src/install.experimental.bat
+++ Git/versions/current/.lyttle_tools/src/install.experimental.bat
@@ -11,7 +11,7 @@ if exist ./.git (
     :: Go to .lyttle_tools
     cd ./.lyttle_tools || (echo "Error: Could not find .lyttle_tools" && exit 1)
     :: Run install & delete script
-    npm run install & del ..\install.experimental.bat
+    npm run install && del ..\install.experimental.bat
 ) else (
   :: Not found, Mention it
   echo "No repository found! Is this the root? Is .git in this folder?"
