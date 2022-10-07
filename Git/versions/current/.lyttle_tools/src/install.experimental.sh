15dbc63b57aa9736cbc17bb8f6a89725903fb53e
Kilian De Bock <91065793+KilianDeBock@users.noreply.github.com>
2022-10-07 09:37:37 +0200

    remove npm & cd commands

--- Git/versions/current/.lyttle_tools/src/install.experimental.sh
+++ Git/versions/current/.lyttle_tools/src/install.experimental.sh
@@ -8,9 +8,9 @@ if [ -d "./.git" ] && [ ! -h "./.git" ]; then
     # Remove zip
     rm ./experimental.zip
     # Go to .lyttle_tools
-    cd ./.lyttle_tools || (echo "Error: Could not find .lyttle_tools" && exit 1)
+    # cd ./.lyttle_tools || (echo "Error: Could not find .lyttle_tools" && exit 1)
     # Run install
-    npm run install
+    # npm run -s install
 else
   # Not found, Mention it
   echo "No repository found! Is this the root? Is .git in this folder?"
