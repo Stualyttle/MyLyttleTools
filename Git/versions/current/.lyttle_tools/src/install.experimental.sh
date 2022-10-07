d7383a7bdf64e0d0e808b0f7e7dac2fe00690e86
Kilian De Bock <91065793+KilianDeBock@users.noreply.github.com>
2022-10-07 08:50:37 +0200

    Add macos/linux experimental sh

---
+++ Git/versions/current/.lyttle_tools/src/install.experimental.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# Check if .git exists
+if [ -d "./.git" ] && [ ! -h "./.git" ]; then
+    # Download latest
+    curl -LO https://github.com/Stualyttle/LyttleTools/raw/main/Git/versions/experimental.zip
+    # Unzip
+    tar -xf ./experimental.zip
+    # Remove zip
+    rm ./experimental.zip
+    # Go to .lyttle_tools
+    cd ./.lyttle_tools || (echo "Error: Could not find .lyttle_tools" && exit 1)
+    # Run install
+    npm run install
+else
+  # Not found, Mention it
+  echo "No repository found! Is this the root? Is .git in this folder?"
+fi
